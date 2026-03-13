896b59ab953fbba3292f07d26dfd92eb7b165b52
iiPython <ben@iipython.dev>
2026-03-13 02:24:33 -0500

    make the video page show publication date

--- snorlax/static/js/video.js
+++ snorlax/static/js/video.js
@@ -15,6 +15,10 @@ if (response.code !== 200) {
     let description = video.description.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     description = description.replace(/(https?:\/\/[^\s]+)/g, `<a href = "$1" target = "_blank" rel = "noopener noreferrer">$1</a>`);
 
+    // Grab date
+    const date = new Date(video.timestamp * 1000);
+    const date_string = `${(date.getMonth() + 1).toString().padStart(2, "0")}/${date.getDate().toString().padStart(2, "0")}/${date.getFullYear()}`;
+
     // Render HTML
     const url = `/videos/${video.channel_id}/${video.id}`;
     MAIN_ELEMENT.innerHTML = `
@@ -25,7 +29,7 @@ if (response.code !== 200) {
         <hr>
         <span>
             <a href = "/channel/${channel.preferred_id}">${channel.name}</a> (${channel.subscribers.toLocaleString()} subscribers) <br>
-            ${video.duration_string} • ${video.view_count.toLocaleString()} views • ${video.like_count.toLocaleString()} likes • ${humanizeTime(video.timestamp)}
+            ${video.duration_string} • ${video.view_count.toLocaleString()} views • ${video.like_count.toLocaleString()} likes • ${date_string} (${humanizeTime(video.timestamp)})
         </span>
         <hr>
         <pre style = "margin-bottom: 20px;">${description}</pre>
