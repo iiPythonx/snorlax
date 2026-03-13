00d0fcd09ee28119cd6510e480a299a3f2212b02
iiPython <ben@iipython.dev>
2026-03-13 02:40:51 -0500

    add search link, use input event instead of keydown, decrease delay

--- snorlax/static/js/search.js
+++ snorlax/static/js/search.js
@@ -1,9 +1,9 @@
 {
     let timeout;
-    document.querySelector("input").addEventListener("keydown", (e) => {
+    document.querySelector("input").addEventListener("input", (e) => {
         if (timeout) clearTimeout(timeout);
         timeout = setTimeout(() => {
             document.querySelector("div[data-api-type]").snorlaxUpdateParams(`query=${e.target.value}`);
-        }, 500);
+        }, 300);
     });
 }
