2599b67cbe0c7ff35078bac39ef974856fa55ecd
iiPython <ben@iipython.dev>
2026-03-20 19:44:04 -0500

    ...

--- snorlax/config.py
+++ snorlax/config.py
@@ -8,8 +8,8 @@ from pydantic import BaseModel
 ROOT = Path(__file__).parent
 
 class SnorlaxConfig(BaseModel):
-    database_path: str
-    video_path:    str
+    database_path: Path
+    video_path:    Path
 
 class VideoConfig(BaseModel):
     subtitle_languages: list[str]
